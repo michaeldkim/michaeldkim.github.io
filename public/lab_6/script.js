66bcd66d4c41822561e58b85cfd2eb40fd77c9f5
Michael <michael_kim57@yahoo.com>
2020-10-24 18:22:10 -0400

    Update script.js

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -2,6 +2,12 @@
 
 const { reverse } = require("cypress/types/lodash");
 
+function getRandomIntInclusive(min, max) {
+  const min1 = Math.ceil(min);
+  const max1 = Math.floor(max);
+  return Math.floor(Math.random() * (max1 - min1 + 1) + min1);
+}
+
 function range(int) {
   const arr = [];
   for (let i = 0; i < int; i += 1) {
